3e526337e5fc1e259abb289f8f4c080f3b7229f6
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-10-28 16:31:44 +0300

    [Feat]: Add Category

---
+++ Core/RedoMusic.Domain/Entities/Category.cs
@@ -0,0 +1,24 @@
+﻿using RedoMusic.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedoMusic.Domain.Entities
+{
+    public class Category : EntityBase<Guid>
+    {
+        public Category(string categoryName)
+        {
+            CategoryName = categoryName;
+        }
+
+        public string CategoryName { get; set; }
+
+        public List<Instrument> InstrumentList { get; set; }
+
+        public List<Brand> BrandList { get; set; }
+
+    }
+}
---
+++ Presentation/RedoMusicMVC/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedoMusic.Domain.Entities;
+using RedoMusic.Persistence.Contexts;
+
+namespace RedoMusicMVC.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly RedoMusicDbcontext dbcontext;
+
+
+        public CategoriesController()
+        {
+            dbcontext = new RedoMusicDbcontext();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetCategory()
+        {
+
+            return View(dbcontext.Categories.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string categoryName)
+        {
+            Category category= new Category(categoryName);
+            dbcontext.Categories.Add(category);
+            dbcontext.SaveChanges();
+
+            return RedirectToAction("AddCategory");
+        }
+
+
+
+
+
+
+    }
+}
