4013bb317a4350068fe96a2db2eee1e56721674b
liva <141638184+livaae@users.noreply.github.com>
2023-10-30 14:45:55 +0300

    [Feat]: Instrument Controller created

---
+++ Presentation/RedoMusicMVC/Controllers/InstrumentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RedoMusicMVC.Controllers
+{
+    public class InstrumentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
