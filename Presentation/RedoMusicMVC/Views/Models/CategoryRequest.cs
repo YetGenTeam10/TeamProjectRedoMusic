b22ec6ae2a1fe7fec864922e8865bf1d19941818
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-11-01 21:00:10 +0300

    [Feat]: Category Update Endpoint

---
+++ Presentation/RedoMusicMVC/Views/Models/CategoryRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RedoMusicMVC.Views.Models
+{
+    public class CategoryRequest
+    {
+        public string CategoryName { get; set; }
+
+        public string? CreatedByUserId { get; set; }
+    }
+}
