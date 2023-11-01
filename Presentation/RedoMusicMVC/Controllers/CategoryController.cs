c67349785c4aacbf7846a149b45a59fc416dcd91
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-11-01 19:13:48 +0300

    [Feat]: Category Add Endpoint

---
+++ Presentation/RedoMusicMVC/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedoMusic.Domain.Entities;
+using RedoMusic.Persistence.Contexts;
+
+namespace RedoMusicMVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly RedoMusicDbcontext dbcontext;
+
+
+        public CategoryController()
+        {
+            dbcontext = new RedoMusicDbcontext();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetCategory()
+        {
+
+            return View(dbcontext.Categories.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string categoryName)
+        {
+            Category category= new Category(categoryName);
+            dbcontext.Categories.Add(category);
+            dbcontext.SaveChanges();
+
+            return RedirectToAction("Add");
+        }
+
+    }
+}
