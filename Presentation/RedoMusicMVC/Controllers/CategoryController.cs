21be7decc619a9dd9e757eec9e99a07bf4d94078
Nejla KÜÇÜK <132754595+nkucukk@users.noreply.github.com>
2023-11-01 20:12:24 +0300

    [Feat]: Category Delete Endpoint

--- Presentation/RedoMusicMVC/Controllers/CategoryController.cs
+++ Presentation/RedoMusicMVC/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RedoMusic.Domain.Entities;
 using RedoMusic.Persistence.Contexts;
 
@@ -11,20 +12,17 @@ namespace RedoMusicMVC.Controllers
 
         public CategoryController()
         {
-            dbcontext = new RedoMusicDbcontext();
-        }
-
-        public IActionResult Index()
-        {
-            return View();
+            dbcontext = new();
         }
 
         [HttpGet]
-        public IActionResult GetCategory()
+        public IActionResult Index()
         {
 
             return View(dbcontext.Categories.ToList());
         }
+
+        //Add Method
 
         [HttpGet]
         public IActionResult Add()
@@ -35,12 +33,35 @@ namespace RedoMusicMVC.Controllers
         [HttpPost]
         public IActionResult Add(string categoryName)
         {
-            Category category= new Category(categoryName);
+            Category category= new ();
+            category.CategoryName = categoryName;
+            category.CreatedOn = DateTime.UtcNow;
+            category.IsDeleted = false;
+            category.CreatedByUserId = "nejlakucuk";
             dbcontext.Categories.Add(category);
             dbcontext.SaveChanges();
 
             return RedirectToAction("Add");
         }
+
+
+        // Delete Method
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            var category= dbcontext.Categories.Where(x => x.Id == Guid.Parse(id)).FirstOrDefault();
+            dbcontext.Categories.Remove(category);
+
+            category.DeletedByUserId = "nejlakucuk";
+            category.DeletedOn = DateTime.UtcNow;
+            category.IsDeleted = true;
+
+            dbcontext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        
 
     }
 }
