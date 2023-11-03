499a3b2017882e8b6c209c64f4f8fb36078e0df9
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-11-03 23:20:11 +0300

    [Feat]: Created InstrumentAddBrandCategory and Brand.Index

---
+++ Presentation/RedoMusicMVC/Models/InstrumentAddBrandCategory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedoMusic.Domain.Entities;
+
+
+namespace RedoMusicMVC.Models
+{
+    public class InstrumentAddBrandCategory 
+    {
+        public Instrument Instrument { get; set; }
+        public List<Brand> Brands { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
