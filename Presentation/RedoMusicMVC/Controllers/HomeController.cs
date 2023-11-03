26b7655e55c645785dbc4833c7757be869e66b6a
Furkan Mert Mısırlı <78733884+mertmisirli@users.noreply.github.com>
2023-11-03 22:54:54 +0300

    index page added

--- Presentation/RedoMusicMVC/Controllers/HomeController.cs
+++ Presentation/RedoMusicMVC/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RedoMusic.Domain.Entities;
 using RedoMusic.Persistence;
+using RedoMusic.Persistence.Contexts;
 using RedoMusicMVC.Models;
 using System.Diagnostics;
 
@@ -8,16 +11,43 @@ namespace RedoMusicMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly RedoMusicDbcontext _redoMusicDbcontext;
 
         public HomeController(ILogger<HomeController> logger)
         {
+            _redoMusicDbcontext = new();
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            var instruments = _redoMusicDbcontext.Instruments.ToList();
+            MyViewModel myViewModel = new MyViewModel();
+
+            // Başka bir sayfada kullanıcı kimlik bilgisine erişim
+            Guid userId;
+            User user = new User();
+            if (HttpContext.Session.GetString("userId") != null)
+            {
+                userId = new Guid(HttpContext.Session.GetString("userId")); // Oturumdan userId'yi al
+                user = _redoMusicDbcontext.Users.FirstOrDefault(u => u.Id == userId);
+
+                if (userId != Guid.Empty)
+                {
+                    // Kullanıcı kimlik bilgisi mevcutsa, işlem yapabilirsiniz
+                }
+                else
+                {
+                    // Kullanıcı kimlik bilgisi mevcut değilse, giriş yapmış bir kullanıcı yoktur.
+                    // Kullanıcıyı giriş sayfasına yönlendirebilirsiniz.
+                }
+            }
+
             Configurations.GetString("ConnectionStrings:PostgreSQL");
-            return View();
+            myViewModel.User = user;
+            myViewModel.Instruments = instruments;
+
+            return View(myViewModel);
         }
 
         public IActionResult Privacy()
@@ -31,4 +61,10 @@ namespace RedoMusicMVC.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
+}
+
+public class MyViewModel
+{
+    public User User { get; set; }
+    public List<Instrument> Instruments { get; set; }
 }
--- Presentation/RedoMusicMVC/Program.cs
+++ Presentation/RedoMusicMVC/Program.cs
@@ -2,9 +2,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddSession();
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,9 +21,12 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
 
 app.Run();
