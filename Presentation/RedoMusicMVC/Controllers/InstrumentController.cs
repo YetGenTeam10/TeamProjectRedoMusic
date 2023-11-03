f4f8fbfb1ce7e501b8af63fb1cb95dd276ce5e68
Livanur Erdem <141638184+livaae@users.noreply.github.com>
2023-11-04 00:37:27 +0300

    [Feat]: Update InstrumentController
    The bug in the add method has been resolved.I also added Delete and Update methods.

--- Presentation/RedoMusicMVC/Controllers/InstrumentController.cs
+++ Presentation/RedoMusicMVC/Controllers/InstrumentController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using RedoMusic.Domain.Entities;
 using RedoMusic.Persistence.Contexts;
+using RedoMusicMVC.Models;
+using RedoMusicMVC.Views.Models;
 
 namespace RedoMusicMVC.Controllers
 {
@@ -20,99 +23,256 @@ namespace RedoMusicMVC.Controllers
             return View(products);
         }
 
-           [HttpGet]
-            public IActionResult Add()
-            {
-                var brands = _dbContext.Brands.ToList();
-                var brandList = new SelectList(brands, "Id", "Name");
-                ViewData["BrandList"] = brandList;
-
-               /*var categories = _dbContext.Categories.ToList();
-                var categoriesList = new SelectList(categories, "Id", "Name");
-                ViewData["CategoriesList"] = categoriesList;*/
-                return View();
-
-            }
-            [HttpPost]
-            public IActionResult Add(Instrument instrument, string brandId, string categoryId)
-            {
-                var category = _dbContext.Categories.Where(x => x.Id == Guid.Parse(categoryId)).FirstOrDefault();
-                /*if (category == null)
-                {
-                    ModelState.AddModelError("instrument.Category.Id", "Invalid Category selected.");
-                    return View(instrument);
-                }*/
-
-
-                var brand = _dbContext.Brands.Where(x => x.Id == Guid.Parse(brandId)).FirstOrDefault();
-
-                instrument.Id = Guid.NewGuid();
-                instrument.Name = $"{instrument.Name}";
-                instrument.Description = $"{instrument.Description}";
-                instrument.Barcode = $"{instrument.Barcode}";
-                instrument.Picture = $"{instrument.Picture}";
-                instrument.ProductionYear = instrument.ProductionYear;
-                instrument.Price = instrument.Price;
-                instrument.Brand = brand;
-                //instrument.Category = category;
-                instrument.CreatedOn = DateTime.UtcNow;
-                instrument.IsDeleted = false;
-                instrument.CreatedByUserId = "LivanurErdem";
-
-                _dbContext.Instruments.Add(instrument);
-                _dbContext.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-            
-
-       /* [HttpGet]
+        //hata alıyorum:
+        [HttpGet]
         public IActionResult Add()
         {
             var brands = _dbContext.Brands.ToList();
-            // var categories = _dbContext.Categories.ToList();
-            return View(brands);
+
+            var categories = _dbContext.Categories.ToList();
+
+            var addInstruments = new InstrumentAddBrandCategory
+            {
+                Brands = brands,
+                Categories = categories,
+                Instrument = new Instrument()
+            };
+
+            return View(addInstruments);
         }
 
+        /*[HttpGet]
+        public IActionResult Add()
+        {
+            var brands = _dbContext.Brands.ToList();
+            var categories = _dbContext.Categories.ToList();
+
+            if (brands == null)
+            {
+                brands = new List<Brand>(); // Brands null ise boş bir liste oluştur.
+            }
+
+            if (categories == null)
+            {
+                categories = new List<Category>(); // Categories null ise boş bir liste oluştur.
+            }
+
+            var addInstruments = new InstrumentAddBrandCategory
+            {
+                Brands = brands,
+                Categories = categories,
+                Instrument = new Instrument()
+            };
+
+            return View(addInstruments);
+        }*/
+
+
         [HttpPost]
-        public IActionResult Add(string name, string description, string brandId, string price, string barcode, string pictureUrl, string categoryId)
+        public IActionResult Add(string name, string description, string brandId, string price, string barcode, string pictureUrl, string categoryId, DateTime? productionYear, string color)
         {
             var brand = _dbContext.Brands.Where(x => x.Id == Guid.Parse(brandId)).FirstOrDefault();
-            //var category = _dbContext.Categories.Where(x => x.Id == Guid.Parse(categoryId)).FirstOrDefault();
-
+            var category = _dbContext.Categories.Where(x => x.Id == Guid.Parse(categoryId)).FirstOrDefault();
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(description) || string.IsNullOrEmpty(barcode) || string.IsNullOrEmpty(price))
+            {
+                // Gerekli alanlar boşsa veya null ise, hata mesajı oluşturun ve geri dönün.
+                ModelState.AddModelError("Validation", "Name, Description, Barcode, and Price are required.");
+                return View();
+            }
             var instrument = new RedoMusic.Domain.Entities.Instrument()
             {
                 Id = Guid.NewGuid(),
                 Name = name,
                 Description = description,
                 Barcode = barcode,
+                Price = decimal.Parse(price),
+                Picture = pictureUrl,  // Burada "Picture" özelliğine bir değer atıyoruz
+                Color = (RedoMusic.Domain.Enums.ColorType)Convert.ToInt32(color),
                 Brand = brand,
-                //Category = category,
+                Category = category,
+                ProductionYear = DateTime.UtcNow,
                 CreatedOn = DateTime.UtcNow,
-                Picture = pictureUrl,
                 IsDeleted = false,
                 CreatedByUserId = "LivanurErdem",
             };
 
+
+
             _dbContext.Instruments.Add(instrument);
 
             _dbContext.SaveChanges();
-
+            
             return RedirectToAction("add");
-        }*/
-
-
-        [Route("[controller]/[action]/{id}")]
-        public IActionResult Delete(string id)
+        }
+        [HttpGet]
+        //Delete method:
+        // [Route("[controller]/[action]/{id}")]
+        public IActionResult DeleteInstrument(string id)
         {
             var instrument = _dbContext.Instruments.Where(x => x.Id == Guid.Parse(id)).FirstOrDefault();
 
             _dbContext.Instruments.Remove(instrument);
 
-            _dbContext.SaveChanges();
+            instrument.DeletedByUserId = "LivanurErdem";
+            instrument.DeletedOn = DateTime.UtcNow;
+            instrument.IsDeleted = true;
 
+            _dbContext.SaveChanges();
+          
             return RedirectToAction("index");
         }
+
+        //Update method:
+        //nejla ile yaptığım:
+       /* [HttpGet]
+        [Route("[controller]/[action]/{id}")]
+        public IActionResult UpdateInstrument([FromRoute] string id)
+        {  
+            InstrumentRequest instrumentRequest = new();
+          
+                var instrument = _dbContext.Instruments.FirstOrDefault(x => x.Id == Guid.Parse(id));
+                if (instrument != null)
+                {
+                    instrument.Name = instrumentRequest.Name;
+                    instrument.Description = instrumentRequest.Description;
+                    instrument.Barcode = instrumentRequest.Barcode;
+                    instrument.ProductionYear = instrumentRequest.ProductionYear;
+                    instrument.Price = instrumentRequest.Price;
+                    instrument.Picture = instrumentRequest.Picture;
+                    instrument.Color = instrumentRequest.Color;
+                    instrument.Brand = instrumentRequest.Brand;
+                    instrument.Category = instrumentRequest.Category;
+
+       
+                }
+
+            instrument.CreatedByUserId = "LivanurErdem";
+
+           var brands = _dbContext.Brands.ToList();
+            var categories = _dbContext.Categories.ToList();
+
+            var addInstrument = new InstrumentAddBrandCategory
+            {
+                Instrument = instrument,
+                Brands = brands,
+                Categories = categories
+            };
+
+         _dbContext.SaveChanges();
+
+          return RedirectToAction("UpdateInstrument");
+      }*/
+
+        //ilk yaptığım
+        /* [HttpGet]
+         public IActionResult UpdateInstrument(Guid id)
+         {
+             var instrument = _dbContext.Instruments.FirstOrDefault(x => x.Id == id);
+             var brands = _dbContext.Brands.ToList();
+             var categories = _dbContext.Categories.ToList();
+
+             var addInstrument = new InstrumentAddBrandCategory
+             {
+                 Instrument = instrument,
+                 Brands = brands,
+                 Categories = categories
+             };
+
+             return View(addInstrument);
+         }
+
+         [HttpPost]
+         public IActionResult UpdateInstrument(Instrument instrument)
+         {
+             if (ModelState.IsValid)
+             {
+                 var existingInstrument = _dbContext.Instruments.FirstOrDefault(x => x.Id == instrument.Id);
+                 if (existingInstrument != null)
+                 {
+                     existingInstrument.Name = instrument.Name;
+                     existingInstrument.Description = instrument.Description;
+                     existingInstrument.Barcode = instrument.Barcode;
+                     existingInstrument.ProductionYear = instrument.ProductionYear;
+                     existingInstrument.Price = instrument.Price;
+                     existingInstrument.Picture = instrument.Picture;
+                     existingInstrument.Color = instrument.Color;
+                     existingInstrument.Brand = instrument.Brand;
+                     existingInstrument.Category = instrument.Category;
+
+                     _dbContext.SaveChanges();
+                 }
+
+                 return RedirectToAction("Index");
+             }
+
+             var brands = _dbContext.Brands.ToList();
+             var categories = _dbContext.Categories.ToList();
+
+             var addInstrument = new InstrumentAddBrandCategory
+             {
+                 Instrument = instrument,
+                 Brands = brands,
+                 Categories = categories
+             };
+
+             return View(addInstrument);
+         }*/
+        [HttpGet]
+        public IActionResult UpdateInstrument(Guid id)
+        {
+            var instrument = _dbContext.Instruments.FirstOrDefault(x => x.Id == id);
+            var brands = _dbContext.Brands.ToList();
+            var categories = _dbContext.Categories.ToList();
+
+            var addInstrument = new InstrumentAddBrandCategory
+            {
+                Instrument = instrument,
+                Brands = brands,
+                Categories = categories
+            };
+
+            return View(addInstrument);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateInstrument(Instrument instrument)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingInstrument = _dbContext.Instruments.FirstOrDefault(x => x.Id == instrument.Id);
+                if (existingInstrument != null)
+                {
+                    existingInstrument.Name = instrument.Name;
+                    existingInstrument.Description = instrument.Description;
+                    existingInstrument.Barcode = instrument.Barcode;
+                    existingInstrument.ProductionYear = instrument.ProductionYear;
+                    existingInstrument.Price = instrument.Price;
+                    existingInstrument.Picture = instrument.Picture;
+                    existingInstrument.Color = instrument.Color;
+                    existingInstrument.Brand = instrument.Brand;
+                    existingInstrument.Category = instrument.Category;
+
+                    _dbContext.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+
+            var brands = _dbContext.Brands.ToList();
+            var categories = _dbContext.Categories.ToList();
+
+            var addInstrument = new InstrumentAddBrandCategory
+            {
+                Instrument = instrument,
+                Brands = brands,
+                Categories = categories
+            };
+
+            return View(addInstrument);
+        }
+
+
+
 
         [HttpGet]
         [Route("[controller]/[action]/{id}")]
